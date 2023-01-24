448d6265193b842d5c499c2ac74a0e238bf11922
yoyo2119 <123515303+yoyo2119@users.noreply.github.com>
2023-01-24 14:21:10 -0800

    Add files via upload

---
+++ addd.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication32;
+import java.util.Scanner;
+
+public class yo {
+      static void sub()
+  {
+   Scanner n=new Scanner(System.in);
+   Scanner m=new Scanner(System.in);
+    int x=n.nextInt();
+     int y=m.nextInt();
+      int sum=x+y;
+      int mul=x*y;
+    
+      System.out.println(sum);
+        System.out.println(mul);
+      
+  }
+    
+
+
+   public static void main(String[] args) {
+       
+       sub();
+
+     
+      
+      
+       
+   
+
+ 
+  }
+   
+}
+
