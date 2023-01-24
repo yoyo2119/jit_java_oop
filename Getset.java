5a34a08038575825ffc8a0fb0237cffe73ed8155
yoyo2119 <123515303+yoyo2119@users.noreply.github.com>
2023-01-24 15:07:14 -0800

    Add files via upload

---
+++ Getset.java
@@ -0,0 +1,37 @@
+
+package javaapplication29;
+
+
+ import java.util.Scanner;
+public class Getset {
+    
+
+
+    
+    public static void main(String[] args) 
+    {
+       
+       
+       System.out.println("adition ended");
+      
+       
+       
+               
+    
+    }
+    public void addition(){
+        Scanner t=new Scanner(System.in);
+        int x,y,sum;
+    System.out.println("enter x");
+      x=t.nextInt();
+      System.out.println("enter y");
+           y=t.nextInt();
+           sum=x+y;
+           System.out.println(sum);
+           
+        
+    }
+    
+    
+    
+}
