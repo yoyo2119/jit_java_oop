25770811e4d798399c545af1ea878275af75fc18
yoyo2119 <123515303+yoyo2119@users.noreply.github.com>
2023-01-24 14:23:44 -0800

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+public class multiplication {
+
+  
+   
+    public void multi(int...args)
+     int product=1
+                    for(int i:args)
+                    product*=i;
+
+return product;
+    
+
+ public static void main(String[] args) {
+     Scanner d = new Scanner(System.in);
+     System.out.println("enter any number:");
+     array[i] = d.nextInt();
+     
+     for(int i=1; i<=10; i++)
+         array[i] = d.nextInt();
+     multi(array);
+     System.out.println("multi");
+}
+}
+
